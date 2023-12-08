48afb6d1728d4653dc0d1d950746d64d367af433
Ramzy Bouziane <rabouzia@made-f0Cr9s6.clusters.42paris.fr>
2023-12-08 15:51:54 +0100

    avancenment

--- get_next_line.c
+++ get_next_line.c
@@ -6,29 +6,47 @@
 /*   By: rabouzia <rabouzia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 15:14:43 by rabouzia          #+#    #+#             */
-/*   Updated: 2023/12/07 16:54:28 by rabouzia         ###   ########.fr       */
+/*   Updated: 2023/12/08 15:48:55 by rabouzia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int ft_strlen_tren(char *str)
+int	not_a_nl(char *str, char c)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (!str)
-        return (0);
-    while(str[i] != '\n')
-        i++;
-    return (i);
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (0);
+		i++;
+	}
+	return (1);
 }
-char substr(); // pour return tt la ligne
-char *strjoin(); // pour coller
-read(); // 
 
-
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    
+	char *buff;
+	static char *stock;
+	int reader;
+	char *tmp;
+
+	if (BUFFER_SIZE < 1 || fd > 1024 || fd < 0)
+		return (NULL);
+	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
+	reader = 1;
+	while (not_a_nl(stock, '\n') == 1 && reader > 0)
+	{
+		reader = read(fd, buff, BUFFER_SIZE);
+		if (reader == -1)
+			return (NULL);
+		tmp = stock;
+		stock = ft_strjoin(tmp, buff);
+	}
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,17 @@
 /*   By: rabouzia <rabouzia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 10:37:24 by rabouzia          #+#    #+#             */
-/*   Updated: 2023/12/07 10:38:23 by rabouzia         ###   ########.fr       */
+/*   Updated: 2023/12/08 10:33:04 by rabouzia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#include <stdlib.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 45
 
+# endif
 
 #endif
